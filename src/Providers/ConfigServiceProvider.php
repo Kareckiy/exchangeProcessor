615ba964851f74959a35a43f84b2a716bc56b3da
Ruslan Karetskiy <kareckiyr@mail.ru>
2021-05-01 15:32:00 +0300

    Main. Change structure

--- src/Providers/ConfigServiceProvider.php
+++ src/Providers/ConfigServiceProvider.php
@@ -9,14 +9,8 @@ class ConfigServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        /*$this->publishes(
-            [
-                __DIR__.'/../Config/exchanges.php' => config_path('exchanges.php'),
-            ]
-        );*/
-
         $this->mergeConfigFrom(
-            __DIR__.'/../Config/exchanges.php', 'exchanges'
+            __DIR__.'/../../config/exchanges.php', 'exchanges'
         );
     }
 
